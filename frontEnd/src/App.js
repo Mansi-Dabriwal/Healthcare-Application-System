9d961abc2d8ebf30ff8507e5c192898835500ade
vktotti <vkirangi1738@gmail.com>
2022-12-06 16:27:58 -0500

    updated app.js

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -4,6 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbarcomp';
 import Home from './pages/Home';
 import About from './pages/About';
+import Testimonials from './pages/Testimonials';
+import Contact from './pages/Contact';
+import PatientSignUp from './pages/PatientSignUp';
+import PatientPortal from './pages/PatientPortal';
+import Settings from './pages/Settings';
+import PatientP from './pages/PatientP';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Logindoc from './pages/Logindoc';
 import LoginPatient from './pages/LoginPatient';
@@ -11,20 +17,22 @@ function App() {
   return (
     <div className="App">
       <Router>
-     
       <Routes>          
                         <Route path='/' element={<Home/>} />
                         <Route path='/home' element={<Home/>} />
                         <Route path='/About' element={<About/>} />
+                        <Route path='/Testimonials' element={<Testimonials/>} />
+                        <Route path='/Contact' element={<Contact/>} />
                         <Route path='/Logindoc' element={<Logindoc/>} />
                         <Route path='/LoginPatient' element={<LoginPatient/>} />
                         <Route path="/patient" element={<PatientPortal />} />
                         <Route path="/settings" element={<Settings />} />
                         <Route path="/patientP" element={<PatientP />} />
+                        <Route path='/PatientSignUp' element={<PatientSignUp/>} />
                         
                         
-     </Routes>
-      </Router>
+    </Routes>
+    </Router>
     </div>
   );
 }
