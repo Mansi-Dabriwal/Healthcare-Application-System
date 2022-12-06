e3cace6744be81430774209738872f5d40ba7347
div31 <divyasuri343@gmail.com>
2022-12-05 21:50:59 -0500

    Added new routes in App.js page

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -7,23 +7,28 @@ import About from './pages/About';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Logindoc from './pages/Logindoc';
 import LoginPatient from './pages/LoginPatient';
+import PatientPortal from './pages/PatientPortal';
+import Settings from './pages/Settings';
+import PatientP from './pages/PatientP';
+import PatientForm from './components/PatientForm'
+
 function App() {
   return (
     <div className="App">
       <Router>
-     
-      <Routes>          
-                        <Route path='/' element={<Home/>} />
-                        <Route path='/home' element={<Home/>} />
-                        <Route path='/About' element={<About/>} />
-                        <Route path='/Logindoc' element={<Logindoc/>} />
-                        <Route path='/LoginPatient' element={<LoginPatient/>} />
-                        <Route path="/patient" element={<PatientPortal />} />
-                        <Route path="/settings" element={<Settings />} />
-                        <Route path="/patientP" element={<PatientP />} />
-                        
-                        
-     </Routes>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Logindoc' element={<Logindoc />} />
+          <Route path='/LoginPatient' element={<LoginPatient />} />
+          <Route path="/patient" element={<PatientPortal />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/patientP" element={<PatientP />} />
+          <Route path="/patient-form" element={<PatientForm />} />
+
+        </Routes>
       </Router>
     </div>
   );
